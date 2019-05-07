5923fedffa13cce8d62c3bae7aa2b5394b7aab90
Joseph Naldo <Joseph.Naldo@gmail.com>
2019-05-07 16:00:41 -0400

    Simple ToDO DOM Scripting Events Part 1 completed

---
+++ app.js
@@ -0,0 +1,56 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    // get the text
+    let title = newToDoText.value;
+
+    // create a new li
+    let newLi = document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+
+    // create a delete button
+    let deleteButton = document.createElement('button');
+
+    // set the delete button text
+    deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener('click', function(event){
+      toDoList.removeChild(this.parentElement);
+    })
+
+    // set the title
+    newLi.textContent = title;
+
+
+
+    // attach the checkbox to the li
+    newLi.appendChild(checkbox);
+
+    // attach the delete button to the li
+    newLi.appendChild(deleteButton);
+
+
+    // attach the li to the ul
+    toDoList.appendChild(newLi);
+
+    //empty the input
+    newToDoText.value = '';
+
+
+
+  });
+ }
+
+window.onload = function() {
+   onReady();
+ };
